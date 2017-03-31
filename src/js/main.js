aed4e8e0258dcfc8e15823d2d78d576a5b89373a
Hideki Abe <hideki.abe.0630@gmail.com>
2017-03-31 20:59:10 +0900

    Delete practice file

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Array.from(document.querySelectorAll('.js-test-01'), function (elem) {
-    console.log(elem.tagName);
-});
-
-var a = { foo: 1 };
-var b = { bar: 2 };
-var c = Object.assign(a, b);
